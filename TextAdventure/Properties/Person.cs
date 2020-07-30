1ee97f77dfd1e009276337eecbfb3e267c35e896
주연 채 <jchae1998@gmail.com>
2020-07-31 03:00:13 +0900

    second

---
+++ TextAdventure/Properties/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Person
+{
+	protected string name{ get; set; }
+	protected int intelligence { get; set; }
+	protected int age { get; set; }
+	protected int money { get; set; }
+	public Person(string name, int intelligence, int age, int money)
+    {
+		this.name = name;
+		this.intelligence = intelligence;
+		this.age = age;
+		this.money = money;
+    }
+}
+
+public class Hasheti : Person
+{
+	int violence;
+	int socialStatus;
+
+	public Hasheti(string name, int intelligence, int age, int money, int violence, int socialStatus) : base(name, intelligence, age, money)
+	{
+		this.violence = violence;
+		this.socialStatus = socialStatus;
+    }
+}
+
+public class Elliyah : Person
+{
+	int niceness;
+	int curiousity;
+	public Elliyah(string name, int intelligence, int age, int money, int niceness, int curiousity) : base(name, intelligence, age, money)
+    {
+		this.niceness = niceness;
+		this.curiousity = curiousity;
+    }
+
+	void niceUp(Elliyah target)
+    {
+		this.niceness += 10;
+		Console.WriteLine("그녀의 착함이 +10 올라갔다!");
+    }
+	
+	void curiousityUp(Elliyah target)
+    {
+		this.curiousity += 10;
+		Console.WriteLine("그녀의 호기심이 +10 올라갔다!");
+    }
+	
+	void IntelligenceUp(Elliyah target)
+    {
+		this.intelligence += 10;
+		Console.WriteLine("그녀의 똑똑함이 +10 올라갔다!");
+    }
+}
+
